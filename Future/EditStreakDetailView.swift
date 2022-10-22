3146d6502e3c8a82f14e15bc8eebc92764f0d590
cjhGeo <chong.jian.hang.geoffrey@sji.edu.sg>
2022-10-22 11:48:11 +0800

    e

---
+++ Future/EditStreakDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  EditStreakDetailView.swift
+//  Future
+//
+//  Created by T Krobot on 22/10/22.
+//
+
+import SwiftUI
+
+struct EditStreakDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditStreakDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditStreakDetailView()
+    }
+}
