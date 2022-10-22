77595160a110b7ed6503955b53815551449150d9
TristanZhu <oxraty@gmail.com>
2022-10-22 11:51:49 +0800

    EditStreak

--- Future/EditStreakDetailView.swift
+++ Future/EditStreakDetailView.swift
@@ -1,20 +1,80 @@
 //
-//  EditStreakDetailView.swift
+//  editStreakDetail.swift
 //  Future
 //
-//  Created by T Krobot on 22/10/22.
+//  Created by hazh2025 on 22/10/22.
 //
 
 import SwiftUI
 
-struct EditStreakDetailView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-struct EditStreakDetailView_Previews: PreviewProvider {
+struct editStreakDetail: View {
+    
+    @State private var date = Date()
+    
+    @State var title = ""
+    @State var startingDate = ""
+    @State var pin = false
+    
+    
+    
+    var body: some View {
+        
+        VStack{
+            Form{
+                Section{
+                    TextField("Title", text: $title)
+                        .multilineTextAlignment(.leading)
+                }
+                
+                Section{
+                    TextField("Starting Date", text: $startingDate)
+                        .multilineTextAlignment(.leading)
+                }
+                
+                Section{
+                    HStack {
+                        Button("Option"){print(1)}
+                        
+                        Spacer()
+                        
+                        Text("Yes")
+                            .foregroundColor(.blue)
+                            .onTapGesture {
+                                pin = true
+                            }
+                        Text("/")
+                        Text("No")
+                            .foregroundColor(.blue)
+                            .onTapGesture {
+                                pin = false
+                            }
+                    }
+                }
+            }
+            Button("Save Event"){}
+            
+            
+            
+        }
+        //            Button("Pick A Date"){
+        //                pickADate()
+        //            }
+        
+    }
+    
+}
+//func pickADate(){
+//        DatePicker(
+//                "Start Date",
+//                selection: $date,
+//                displayedComponents: [.date]
+//            )
+//        .datePickerStyle(.graphical)
+//    }
+
+struct editStreakDetail_Previews: PreviewProvider {
     static var previews: some View {
-        EditStreakDetailView()
+        editStreakDetail()
     }
 }
