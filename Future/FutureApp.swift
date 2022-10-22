2391e1f18324b0a3b9b0c1484f6bcc98649b42e0
cjhGeo <chong.jian.hang.geoffrey@sji.edu.sg>
2022-10-22 10:06:27 +0800

    Initial Commit

---
+++ Future/FutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureApp.swift
+//  Future
+//
+//  Created by T Krobot on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
