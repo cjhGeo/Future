a86db1dfa4afcef723efaf52969a67673ec17738
cjhGeo <chong.jian.hang.geoffrey@sji.edu.sg>
2022-11-18 14:12:23 +0800

    Added the equivalent of EditButton()

--- Future/CountdownsView.swift
+++ Future/CountdownsView.swift
@@ -17,60 +17,99 @@ struct CountdownsView: View {
     
     @State var scam = false
     
-    @Environment(\.editMode) private var editMode
+    @State var editMode = EditMode.active
+    
+    @State var presentation = true
     
     //    @StateObject var todoManager = TodoManager()
     
     var body: some View {
         NavigationView{
             
-            GeometryReader { geometry in
-                VStack {
-                    LazyVGrid(columns: columns, spacing: 50) {
-                        ForEach($events) { $event in
-                            if !event.isCompleted {
-                                NavigationLink{
-                                    StreakDetailView(events: $event)
-                                } label: {
-                                    ZStack {
-                                        Circle()
-                                            .foregroundColor(event.colour == "red" ? .red : event.colour == "blue" ? .blue : .green)
-                                            .opacity(0.5)
-                                            .frame(width: geometry.size.width/3 - 15 ,
-                                                   height: geometry.size.width/3 - 15)
-                                        
-                                        Image(systemName: "pencil.circle.fill")
-                                            .resizable()
-                                            .frame(width: 40, height: 40)
-                                            .foregroundColor(.white)
+            if presentation {
+                GeometryReader { geometry in
+                    VStack {
+                        LazyVGrid(columns: columns, spacing: 50) {
+                            ForEach($events) { $event in
+                                if !event.isCompleted {
+                                    NavigationLink{
+                                        StreakDetailView(events: $event)
+                                    } label: {
+                                        ZStack {
+                                            Circle()
+                                                .foregroundColor(event.colour == "red" ? .red : event.colour == "blue" ? .blue : .green)
+                                                .opacity(0.5)
+                                                .frame(width: geometry.size.width/3 - 15 ,
+                                                       height: geometry.size.width/3 - 15)
+                                            
+                                            Image(systemName: "pencil.circle.fill")
+                                                .resizable()
+                                                .frame(width: 40, height: 40)
+                                                .foregroundColor(.white)
+                                        }
                                     }
                                 }
                             }
                         }
                     }
+                    .padding()
                 }
-                .padding()
-            }
-            .frame(height: 600)
-            .navigationTitle("Countdowns")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        isSheetPresented = true
-                    } label: {
-                        Image(systemName: "plus")
+                .frame(height: 600)
+                .navigationTitle("Countdowns")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            isSheetPresented = true
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            presentation = false
+                            editMode = EditMode.active
+                        } label : {
+                            Text("Edit")
+                        }
                     }
                 }
-                
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
+                .sheet(isPresented: $isSheetPresented) {
+                    EditStreakDetailView(events: $events)
+                    
                 }
-            }
-            .sheet(isPresented: $isSheetPresented) {
-                EditStreakDetailView(events: $events)
+                .environment(\.editMode, $editMode)
                 
+            } else {
+                List {
+                    ForEach($events) { $event in
+                        if !event.isCompleted {
+                            NavigationLink{
+                                StreakDetailView(events: $event)
+                            } label: {
+                                Text(event.title)
+                            }
+                        }
+                    }
+                    .onDelete { indexSet in
+                        events.remove(atOffsets: indexSet)
+                    }
+                    .onMove { originalOffset, newOffset in
+                        events.move(fromOffsets: originalOffset, toOffset: newOffset)
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            presentation = true
+                            editMode = EditMode.inactive
+                        } label : {
+                            Text("Done")
+                        }
+                    }
+                }
+                .environment(\.editMode, $editMode)
             }
-            
             
             
             
