e3ca38a6e3febf2d74328119acdbc0ebff8ed256
Wu Niutian <wuniutian@gmail.com>
2022-11-24 16:31:23 +0800

    Added ScreenshotMaker and ScreenshotMakerView

---
+++ Future/ScreenshotMaker .swift
@@ -0,0 +1,22 @@
+//
+//  ScreenshotMaker .swift
+//  Future
+//
+//  Created by    wuniutian on 24/11/22.
+//
+
+import UIKit
+
+class ScreenshotMaker: UIView {
+    /// Takes the screenshot of the superview of this superview
+    /// - Returns: The UIImage with the screenshot of the view
+    func screenshot() -> UIImage? {
+        guard let containerView = self.superview?.superview,
+              let containerSuperview = containerView.superview else { return nil }
+        let renderer = UIGraphicsImageRenderer(bounds: containerView.frame)
+        return renderer.image { (context) in
+            containerSuperview.layer.render(in: context.cgContext)
+        }
+    }
+}
+
